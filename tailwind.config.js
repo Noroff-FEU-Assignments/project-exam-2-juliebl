7558c2ac9f47973a3f431112166922cb2f5675e4
juliebl <juliebernadette@gmail.com>
2021-04-20 18:55:55 +0200

    extend theme with animations and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,55 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: {
-        light: '#FF612A',
-        DEFAULT: '#FF4606',
-        dark: '#FF1A00',
+    extend: {
+      colors: {
+        primary: {
+          light: '#FF612A',
+          DEFAULT: '#FF4606',
+          dark: '#FF1A00',
+        },
+        neutral: {
+          light: '#F6F7F8',
+          DEFAULT: '#5c6ac4',
+          dark: '#202e78',
+        },
+        secondary: {
+          light: '#EEF2FF',
+          DEFAULT: '#001D2F',
+          dark: '#202e78',
+        },
+        success: {
+          DEFAULT: '#E0F1E7',
+          dark: '#0E6630',
+        },
+        warning: {
+          DEFAULT: '#FFF89C',
+          dark: '#B8850F',
+        },
+        danger: {
+          DEFAULT: '#FFBDAC',
+          dark: '#BC0000',
+        },
+        loading: {
+          light: '#FFF',
+          DEFAULT: '#F6F7F8',
+          dark: '#153044',
+        },
       },
-      neutral: {
-        light: '#F6F7F8',
-        DEFAULT: '#5c6ac4',
-        dark: '#202e78',
+      animation: {
+        'spin-reverse': 'spin-reverse 2s linear infinite',
       },
-      secondary: {
-        light: '#EEF2FF',
-        DEFAULT: '#001D2F',
-        dark: '#202e78',
+      keyframes: {
+        'spin-reverse': {
+          '0%': {
+            transform: 'rotate(-90deg), rotate(-89deg)',
+          },
+          '100%': { transform: 'rotate(-357deg)' },
+        },
       },
-      success: {
-        DEFAULT: '#E0F1E7',
-        dark: '#0E6630',
-      },
-      warning: {
-        DEFAULT: '#FFF89C',
-        dark: '#B8850F',
-      },
-      danger: {
-        DEFAULT: '#FFBDAC',
-        dark: '#FF3400',
-      },
-      // Configure your color palette here
     },
-    extend: {},
   },
+
   variants: {
     extend: {},
   },
