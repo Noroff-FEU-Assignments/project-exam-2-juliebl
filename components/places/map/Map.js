c750fa33f06c490ff799d8fd1f4fecdc7310baa5
juliebl <juliebernadette@gmail.com>
2021-04-23 21:47:34 +0200

    uninstall mapbox-gl and install react-map-gl

--- components/places/map/Map.js
+++ components/places/map/Map.js
@@ -1,50 +1,11 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+import React from 'react';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-
-const Map = () => {
-  const mapContainerRef = useRef(null);
-
-  const [lng, setLng] = useState(5.32221);
-  const [lat, setLat] = useState(60.39289);
-  const [zoom, setZoom] = useState(11);
-
-  // Initialize map when component mounts
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
-    map.on('load', function () {
-      map.resize();
-    });
-    // Clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
+function Map() {
   return (
     <div>
-      <div>
-        <div>
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
-      </div>
-      <div ref={mapContainerRef} />
+      <p>map</p>
     </div>
   );
-};
+}
 
 export default Map;
