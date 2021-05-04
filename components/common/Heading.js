193723cbb09035aeb31302945bf8c99050711e14
juliebl <juliebernadette@gmail.com>
2021-05-04 18:28:26 +0200

    add top margin

--- components/common/Heading.js
+++ components/common/Heading.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 function Heading({ text }) {
-  return <h1 className="font-bold text-4xl mb-10 text-center">{text}</h1>;
+  return <h1 className="font-bold text-4xl mt-20 mb-10 text-center">{text}</h1>;
 }
 
 Heading.propTypes = {
