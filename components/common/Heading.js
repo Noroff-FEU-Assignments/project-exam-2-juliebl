f3ab7df60779ac07b7cd80db96f3528d28988e41
juliebl <juliebernadette@gmail.com>
2021-05-06 01:33:55 +0200

    add smaller heading

--- components/common/Heading.js
+++ components/common/Heading.js
@@ -4,7 +4,14 @@ function Heading({ text }) {
   return <h1 className="font-bold text-4xl mt-20 mb-10 text-center">{text}</h1>;
 }
 
+export function HeadingSmaller({ text }) {
+  return <h1 className="font-bold text-3xl mt-6">{text}</h1>;
+}
+
 Heading.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+HeadingSmaller.propTypes = {
   text: PropTypes.string.isRequired,
 };
 
