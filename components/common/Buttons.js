2502449bbc1ad2782335ea034fd83bd2930bf2ce
juliebl <juliebernadette@gmail.com>
2021-05-23 16:49:57 +0200

    remove outline on focus, add active class

--- components/common/Buttons.js
+++ components/common/Buttons.js
@@ -2,7 +2,7 @@ export function PrimaryButton({ type, children }) {
   return (
     <button
       type={type}
-      className="text-white shadow transition  bg-primary hover:bg-primary-light rounded-md py-2 px-4 m-0 flex items-center">
+      className="text-white shadow transition  bg-primary hover:bg-primary-light focus:outline-none active:shadow-none active:bg-primary rounded-md py-2 px-4 m-0 flex items-center">
       {children}
     </button>
   );
